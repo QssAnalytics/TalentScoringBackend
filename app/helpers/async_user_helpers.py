f24f279c73feb7d6957158b29081bfd5be3064c8
Tamerlan Aliyev <tamerlan-aliyev-2021@mail.ru>
2023-07-26 17:24:10 +0400

    update questions

--- app/helpers/async_user_helpers.py
+++ app/helpers/async_user_helpers.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 UserAccount = get_user_model()
 user_account_type = TypeVar('user_account_type', bound=UserAccount)
 
-def get_date_weight(finnly_date):
+async def get_date_weight(finnly_date):
         if 0 <= finnly_date < 1:
                 finnly_date_weight = 0.9
         elif 1 <= finnly_date < 3:
@@ -23,7 +23,7 @@ def get_date_weight(finnly_date):
 
         return finnly_date_weight
 
-def calculate_date_difference(data):
+async def calculate_date_difference(data):
         if data["endDate"] == "":
                 current_date = datetime.now()
                 start_date = datetime.strptime(data["startDate"], "%Y-%m-%d")
@@ -35,7 +35,7 @@ def calculate_date_difference(data):
         return difference
 
 
-def get_education_score(user: user_account_type):
+async def get_education_score(user: user_account_type):
         tehsil_score = 0
         
         education= user.user_info[0]["formData"]["education"]
@@ -68,7 +68,7 @@ def get_education_score(user: user_account_type):
         return tehsil_score
 
 
-def get_experience_score(user: user_account_type):
+async def get_experience_score(user: user_account_type):
         userdata = user.user_info[3]["formData"]["experiences"]
         experiance_score = 1
         if len(userdata)>0:
@@ -82,34 +82,25 @@ def get_experience_score(user: user_account_type):
                                         max = workingform[data["workingActivityForm"]["answer"]]
                                         max_working_form_weight = data["workingActivityForm"]["answer_weight"]
                                         profession_degree_weight = data["degreeOfProfes"]["answer_weight"]
-                                        difference = calculate_date_difference(data)
+                                        difference = await calculate_date_difference(data)
 
                         finnly_date = difference.days/365.25
-                        finnly_date_weight = get_date_weight(finnly_date=finnly_date)
+                        finnly_date_weight = await get_date_weight(finnly_date=finnly_date)
                         experiance_score = max_working_form_weight*profession_degree_weight* finnly_date_weight
                 else:
                         max_working_form_weight = userdata[0]["workingActivityForm"]["answer_weight"]
                         profession_degree_weight = userdata[0]["degreeOfProfes"]["answer_weight"]
-                        difference = calculate_date_difference(userdata[0])
+                        difference = await calculate_date_difference(userdata[0])
                         finnly_date = difference.days/365.25
-                        finnly_date_weight = get_date_weight(finnly_date=finnly_date)
+                        finnly_date_weight = await get_date_weight(finnly_date=finnly_date)
                         experiance_score = max_working_form_weight*profession_degree_weight* finnly_date_weight
 
                 return experiance_score
         return experiance_score
 
-def get_language_score():
-        pass
 
-    # end_time = time.time()
-        # duration = end_time - start_time
-        # print("Duration:", duration, "seconds")
-        # tehsil_score = (occupation_weight*education_weight*(grade_weight*education_score*olympiad_status_weight*olympiad_rank_weight)**(1/3))*100
-# education_weight = np.exp(np.log(education[0]["bachelor"]["answer_weight"]) + np.log(education[1]["master"]["answer_weight"]) + np.log(education[2]["phd"]["answer_weight"]))
-        # start_time = time.time()
+async def get_skills_score(user):
 
-
-def get_skills_weight(user):
         userdata = user.user_info[4]["formData"]["specialSkills"]
         lst=[]
         heveskar_count = 0
@@ -130,3 +121,14 @@ def get_skills_weight(user):
 
         formula_result = (heveskar_count**heveskar_answer_weight) * (pesekar_count**pesekar_answer_weight)
         return formula_result
+
+async def get_language_score(user):
+
+        userdata = user.user_info[5]["formData"]["languageSkills"]
+        total_language_weight = 1
+        if len(userdata) > 0:
+                for data in userdata:
+                        total_language_weight *= data['answer_weight']
+                return total_language_weight
+        
+        return total_language_weight
